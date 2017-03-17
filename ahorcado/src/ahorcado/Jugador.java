a1358bbcad664f532f4cc26b20d4fac17e9ff10e
DANIELGG98 <dagonzalez9804@gmail.com>
2017-03-17 17:34:17 -0500

    Add files via upload

---
+++ ahorcado/src/ahorcado/Jugador.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ahorcado;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Jugador {
+    
+    char[] palabra = new char[7];
+    
+    
+
+    public Jugador() {
+        
+        palabra[0]='s';
+        palabra[1]='a';
+        palabra[2]='n';
+        palabra[3]='t';
+        palabra[4]='a';
+        palabra[5]='f';
+        palabra[6]='e';
+        
+        
+        
+        
+    }
+
+    
+}
